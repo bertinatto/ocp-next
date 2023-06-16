3b8c820ed88c712be19663a0635a7f2599e7cf0a
Fabio Bertinatto <fbertina@redhat.com>
2023-06-16 13:31:46 -0300

    Script improvements

--- do.sh
+++ do.sh
@@ -1,40 +1,77 @@
 #!/bin/bash
 
-LOG="$(realpath output.log)"
+set -eo pipefail
 
-# First, import repos
-git clone --origin upstream https://github.com/kubernetes/kubernetes.git
-pushd kubernetes || exit
-git remote add openshift --fetch https://github.com/openshift/kubernetes.git
+main() {
+    local branch_name="$1"
 
-# Create a "ocp-next" branch and make sure it's clean
-git checkout -B ocp-next upstream/master
-git clean -fd
-git checkout -- .
+    pushd kubernetes || exit 1
+    clone_repos
+    create_branch "$branch_name"
+    merge_changes
+    apply_patches
+    update_dependencies
+    update_vendor
+    update_and_build
+    commit_and_push
+    popd
+}
 
-# Create a merge commit to bring the openshift changes to our branch
-git merge -s ours --no-edit openshift/master
+clone_repos() {
+    git clone --origin upstream "https://github.com/kubernetes/kubernetes.git" || true
+    git remote add openshift --fetch "https://github.com/openshift/kubernetes.git" || true
+}
 
-# Apply each patch individually. If one of them fail, abort immediately
-echo "Starting: $(date -u +%Y-%m-%dT%H:%M:%S%Z)" > "$LOG"
-for patch in ../patches/*.patch; do
-    if ! git am "$patch" >> "$LOG" 2>&1; then
-        echo "Failed to apply $patch. Check the log at $LOG"
-        exit 1
-    else
-        echo "Applied $patch"
-    fi
-done
+create_branch() {
+    local branch_name="$1"
+    git clean -fd
+    git checkout -- .
+    git checkout -B "$branch_name" upstream/master
+}
 
-# No patches failed, so update the dependencies
+merge_changes() {
+    git merge -s ours --no-edit openshift/master
+}
 
-GOPROXY=direct hack/pin-dependency.sh github.com/onsi/ginkgo/v2=github.com/openshift/onsi-ginkgo/v2 v2.9-openshift-4.14
-GOPROXY=direct hack/pin-dependency.sh github.com/openshift/api=github.com/bertinatto/api ocp-next
-GOPROXY=direct hack/pin-dependency.sh github.com/openshift/client-go=github.com/bertinatto/client-go ocp-next
-GOPROXY=direct hack/pin-dependency.sh github.com/openshift/library-go=github.com/bertinatto/library-go ocp-next
-GOPROXY=direct hack/pin-dependency.sh github.com/openshift/apiserver-library-go=github.com/bertinatto/apiserver-library-go ocp-next
+apply_patches() {
+    local patch
+    echo "Starting to apply patches"
+    for patch in ../patches/*.patch; do
+        if ! git am "$patch"; then
+            echo "Failed to apply $patch"
+	    exit 1
+        else
+            echo "Applied $patch"
+        fi
+    done
+}
 
-hack/update-vendor.sh
+update_dependencies() {
+    GOPROXY=direct hack/pin-dependency.sh github.com/onsi/ginkgo/v2=github.com/openshift/onsi-ginkgo/v2 v2.9-openshift-4.14
+    GOPROXY=direct hack/pin-dependency.sh github.com/openshift/api=github.com/bertinatto/api ocp-next
+    GOPROXY=direct hack/pin-dependency.sh github.com/openshift/client-go=github.com/bertinatto/client-go ocp-next
+    GOPROXY=direct hack/pin-dependency.sh github.com/openshift/library-go=github.com/bertinatto/library-go ocp-next
+    GOPROXY=direct hack/pin-dependency.sh github.com/openshift/apiserver-library-go=github.com/bertinatto/apiserver-library-go ocp-next
+}
 
-export PATH="/home/fbertina/src/k8s.io/kubernetes/third_party/etcd:${PATH}"
-sudo PATH="$PATH" make update
+update_vendor() {
+    hack/update-vendor.sh
+}
+
+update_and_build() {
+    eval "$(hack/install-etcd.sh | grep "export PATH")"
+    # FIXME: this is for local testing
+    sudo PATH="$PATH" make clean
+    sudo PATH="$PATH" make update
+}
+
+commit_and_push() {
+    git add .
+    git commit -m "UPSTREAM: <drop>: update dependencies and generated files"
+    git remote add origin --fetch "https://github.com/bertinatto/kubernetes.git" || true
+    git push origin HEAD -f
+}
+
+if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
+    main "$@"
+fi
